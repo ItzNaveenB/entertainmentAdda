5c40a685bf68b36a14ef792c7bf7b275a56b077c
Naveen <naveenbaghel5429@gmail.com>
2023-03-21 23:00:33 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import SimpleBottomNavigation from "./components/MainNav";
+import Movies from "./Pages/Movies/Movies";
+import Series from "./Pages/Series/Series";
+import Trending from "./Pages/Trending/Trending";
+import Search from "./Pages/Search/Search";
+import { Container } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className="app">
+        <Container>
+          <Routes>
+            <Route path="/" element={<Trending/>} exact />
+            <Route path="/movies" element={<Movies/>} />
+            <Route path="/series" element={<Series/>} />
+            <Route path="/search" element={<Search/>} />
+          </Routes>
+        </Container>
+      </div>
+      <SimpleBottomNavigation />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import "./Header.css";
+
+const Header = () => {
+    return <span onClick={() => window.scroll(0,0)} className="header">🎬Entertainment Adda📽️</span>
+};
+
+export default Header;
